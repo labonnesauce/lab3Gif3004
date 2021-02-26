968a3f0654edbda5bbf150cac8d7c435e5a45d7c
setr <setr@localhost.localdomain>
2021-02-26 11:51:01 -0500

    un peu de code

--- labo3-h21/src/commMemoirePartagee.c
+++ labo3-h21/src/commMemoirePartagee.c
@@ -2,25 +2,31 @@
 #include <sys/stat.h>        /* For mode constants */
 #include <fcntl.h>           /* For O_* constants */
 #include <sys/mman.h>
+#include <pthread.h>
 
 // Appelé au début du programme pour l'initialisation de la zone mémoire (cas du lecteur)
 int initMemoirePartageeLecteur(const char* identifiant, struct memPartage *zone){
     shm_open(identifiant, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR); //peut etre enlever Write mode
     ftruncate(zone->fd, zone->tailleDonnees);
 
-    mmap(NULL, zone->tailleDonnees, PROT_READ, MAP_SHARED, zone->fd, 4) //addresse? offset de 4 ou 0?
+    mmap(NULL, zone->tailleDonnees, PROT_READ, MAP_SHARED, zone->fd, 4); //addresse? offset de 4 ou 0?
 }
 
 // Appelé au début du programme pour l'initialisation de la zone mémoire (cas de l'écrivain)
 int initMemoirePartageeEcrivain(const char* identifiant, struct memPartage *zone, size_t taille, struct memPartageHeader* headerInfos){
-        shm_open(identifiant, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR); //peut etre enlever Read mode
-        ftruncate(zone->fd, zone->tailleDonnees);
+    shm_open(identifiant, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR); //peut etre enlever Read mode
+    ftruncate(zone->fd, zone->tailleDonnees);
 
-        mmap(NULL, zone->tailleDonnees, PROT_WRITE, MAP_SHARED, zone->fd, 4) //addresse? offset de 4 ou 0?
+    mmap(NULL, zone->tailleDonnees, PROT_WRITE, MAP_SHARED, zone->fd, 4); //addresse? offset de 4 ou 0?
 }
 
 // Appelé par le lecteur pour se mettre en attente d'un résultat
 int attenteLecteur(struct memPartage *zone){
+    pthread_mutex_t count_mutex;
+    pthread_mutex_lock(&count_mutex);
+    if(fstat != 0){
+        pthread_mutex_unlock(&count_mutex);
+    }
 }
 
 // Fonction spéciale similaire à attenteLecteur, mais asynchrone : cette fonction ne bloque jamais.
