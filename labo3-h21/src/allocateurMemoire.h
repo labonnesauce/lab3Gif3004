cf9296b4ca830657921df8deb30313893ed5d9d7
labonnesauce <35840988+labonnesauce@users.noreply.github.com>
2021-03-06 10:45:17 -0500

    Update allocateurMemoire.h

--- labo3-h21/src/allocateurMemoire.h
+++ labo3-h21/src/allocateurMemoire.h
@@ -16,27 +16,16 @@
 #define ALLOC_SMALL_SIZE 1024
 
 
-typedef struct poolFreed{
-	struct poolFreed *nextFree;
-} poolFreed;
-
-struct memPool
-{
-    poolFreed *freed;
-    char* m_poolPtr;
-    char* m_nextAvailAddr;
-    char* m_endAddr;
-};
 // Prépare les buffers nécessaires pour une allocation correspondante aux tailles
 // d'images passées en paramètre. Retourne 0 en cas de succès, et -1 si un
 // problème (par exemple manque de mémoire) est survenu.
-int prepareMemoire(size_t tailleImageEntree, size_t tailleImageSortie, struct memPool* pool);
+int prepareMemoire(size_t tailleImageEntree, size_t tailleImageSortie);
 
 // Ces deux fonctions doivent pouvoir s'utiliser exactement comme malloc() et free()
 // (dans la limite de la mémoire disponible, bien sûr)
-void* tempsreel_malloc(size_t taille, struct memPool* pool);
+void* tempsreel_malloc(size_t taille);
 
-void tempsreel_free(void* ptr, struct memPool* pool);
+void tempsreel_free(void* ptr);
 
 // N'oubliez pas de créer le fichier allocateurMemoire.c et d'y implémenter les fonctions décrites ici!
 
