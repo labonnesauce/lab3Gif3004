b72738c64fc5effa982f915f759efd33eebe9d2f
labonnesauce <35840988+labonnesauce@users.noreply.github.com>
2021-03-06 11:06:05 -0500

    Update commMemoirePartagee.c

--- labo3-h21/src/commMemoirePartagee.c
+++ labo3-h21/src/commMemoirePartagee.c
@@ -6,60 +6,77 @@
 
 // Appelé au début du programme pour l'initialisation de la zone mémoire (cas du lecteur)
 int initMemoirePartageeLecteur(const char* identifiant, struct memPartage *zone){
-    struct stat fileInfo;
-    fstat(zone->fd, &fileInfo);
+    while ((zone->fd = shm_open(identifiant, O_RDWR, S_IRUSR | S_IWUSR)) < 0);
+    struct stat st;
+    do{
+        fstat(zone->fd, &st);
+    }while(st.st_size == 0);
 
-    if(fileInfo.st_size){ //­>0
-        if(zone->header->frameWriter == 0){
-            sched_yield();
-        }else{
+    void* shm = mmap(NULL, st.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, zone->fd, 0);
+    memPartageHeader* header = (memPartageHeader*)shm;
 
-        }
-    }
+    zone->data = (((unsigned char*)shm)+sizeof(memPartageHeader));
+    zone->header = header;
+    zone->tailleDonnees = st.st_size - sizeof(memPartageHeader);
+    zone->copieCompteur = 0;
 
-    shm_open(identifiant, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR); //peut etre enlever Write mode
+    while(zone->header->frameWriter == 0);
+
+    return 0;
+
 }
 
 // Appelé au début du programme pour l'initialisation de la zone mémoire (cas de l'écrivain)
 int initMemoirePartageeEcrivain(const char* identifiant, struct memPartage *zone, size_t taille, struct memPartageHeader* headerInfos){
-    shm_open(identifiant, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR); //peut etre enlever Read mode
-    ftruncate(zone->fd, zone->tailleDonnees);
+     if ((zone->fd = shm_open(identifiant, O_CREAT | O_RDWR,  S_IRUSR | S_IWUSR)) < 0){
+           return -1;
+     }
 
-    mmap(NULL, zone->tailleDonnees, PROT_WRITE, MAP_SHARED, zone->fd, 4); //addresse? offset de 4 ou 0?
+    ftruncate(zone->fd,taille+sizeof(memPartageHeader));
+
+    void* shm = mmap(NULL,taille, PROT_WRITE | PROT_READ , MAP_SHARED, zone->fd, 0); //addresse? offset de 4 ou 0?
+    memPartageHeader* header = (memPartageHeader*)shm;
+    
+    pthread_mutexattr_t mutex_attr;
+    pthread_mutexattr_init(&mutex_attr);
+
+    pthread_mutexattr_setpshared(&mutex_attr, PTHREAD_PROCESS_SHARED);
+
+    pthread_mutex_init(&header->mutex, &mutex_attr);
+
+    zone->data = (((unsigned char*)shm)+sizeof(memPartageHeader));
+    zone->header = header;
+    zone->tailleDonnees = taille;
+    zone->copieCompteur = 99999;
+    
+    header->largeur = headerInfos->largeur;
+    header->hauteur = headerInfos->hauteur;
+    header->canaux = headerInfos->canaux;
+    header->fps = headerInfos->fps;
+    header->frameReader = 0;
+    header->frameWriter = 0;
+
+    return 0;
 }
 
 // Appelé par le lecteur pour se mettre en attente d'un résultat
 int attenteLecteur(struct memPartage *zone){
-
-    if(zone->copieCompteur == 0){
-        sched_yield();
-    }else{
-        pthread_mutex_lock(&zone->header->mutex);
-        zone->header->frameReader++;
-    }
-    return 0;
+    while(zone->header->frameWriter == zone->header->frameReader); 
+    return pthread_mutex_lock(&zone->header->mutex);
 }
 
 // Fonction spéciale similaire à attenteLecteur, mais asynchrone : cette fonction ne bloque jamais.
 // Cela est utile pour le compositeur, qui ne doit pas bloquer l'entièreté des flux si un seul est plus lent.
 int attenteLecteurAsync(struct memPartage *zone){
-    if(zone->copieCompteur == 0){
-        sched_yield();
-    }else{
-        pthread_mutex_lock(&(zone->header->mutex));
-        zone->header->frameReader++;
-    }
-    return 0;
+    if(zone->header->frameWriter == zone->header->frameReader)
+        return 1;
+
+    return pthread_mutex_trylock(&zone->header->mutex);
 
 }
 
 // Appelé par l'écrivain pour se mettre en attente de la lecture du résultat précédent par un lecteur
 int attenteEcrivain(struct memPartage *zone){
-    if(zone->copieCompteur == zone->header->frameWriter){
-        sched_yield();
-    }else{
-        pthread_mutex_lock(&(zone->header->mutex));
-        zone->header->frameReader++;
-    }
-    return 0;
+   while(zone->copieCompteur == zone->header->frameReader);
+   return pthread_mutex_lock(&zone->header->mutex);
 }
