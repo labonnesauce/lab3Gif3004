682f71513bc9944ef0cbf2a64a3cfa8ecb7e6e23
labonnesauce <35840988+labonnesauce@users.noreply.github.com>
2021-03-06 10:54:25 -0500

    Update allocateurMemoire.c

--- labo3-h21/src/allocateurMemoire.c
+++ labo3-h21/src/allocateurMemoire.c
@@ -8,44 +8,47 @@
 #define ALLOC_N_BIG 16
 #define ALLOC_N_SMALL 100
 #define ALLOC_SMALL_SIZE 1024
+#define ESPACE_MEMOIRE 5
+
+static void* espace[ESPACE_MEMOIRE];
+static uint8_t libre[ESPACE_MEMOIRE];
 
 
 // Prépare les buffers nécessaires pour une allocation correspondante aux tailles
 // d'images passées en paramètre. Retourne 0 en cas de succès, et -1 si un
 // problème (par exemple manque de mémoire) est survenu.
-int prepareMemoire(size_t tailleImageEntree, size_t tailleImageSortie, struct memPool* pool){
-    //code trouvé ici https://stackoverflow.com/questions/11749386/implement-own-memory-pool
-    pool->m_poolPtr = (char *) malloc(tailleImageSortie + tailleImageEntree * 5);
-
-    if(pool->m_poolPtr == NULL)
-    {
-         return -1;
+int prepareMemoire(size_t tailleImageEntree, size_t tailleImageSortie){
+    size_t allocate = tailleImageEntree;
+    if (allocate < tailleImageSortie) {
+        allocate = tailleImageSortie;
     }
-    
-    pool->m_nextAvailAddr = pool->m_poolPtr;
-    /** note the addressing starts from zero - thus you have already counted zero**/
-    pool->m_endAddr = pool->m_poolPtr + tailleImageEntree - 1;
+
+    for(int i = 0; i<ESPACE_MEMOIRE; i++){ 
+        espace[i] = malloc(allocate);
+        libre[i] = 1;
+    }
     return 0;
 }
 
 // Ces deux fonctions doivent pouvoir s'utiliser exactement comme malloc() et free()
 // (dans la limite de la mémoire disponible, bien sûr)
-void* tempsreel_malloc(size_t taille, struct memPool* pool){
-    void *mem = NULL;
-    if((pool->m_endAddr != NULL) && (pool->m_nextAvailAddr != NULL)){
-        /** This is according to fencing problem - add 1 when you are find a difference of sequence to calculate the space within **/
-        size_t availableSize = pool->m_endAddr - pool->m_nextAvailAddr + 1;
-
-        if(taille > availableSize){
-            mem = NULL;
-        }else{
-            mem = pool->m_nextAvailAddr;
-            pool->m_nextAvailAddr += taille;
+void* tempsreel_malloc(size_t taille){
+    for(int i = 0; i < (ESPACE_MEMOIRE - 1); i++){
+        if(libre[i] == 1){
+            libre[i] = 0;
+            return espace[i];
         }
     }
-    return mem;
+    perror("Erreur leur du tempsreel_malloc \n", taille);
+    return NULL;
 }
 
-void tempsreel_free(void* ptr, struct memPool* pool){
-   free(ptr);
+void tempsreel_free(void* ptr){
+   for(int i = 0; i< (ESPACE_MEMOIRE-1); i++){ 
+        if(epace[i] == ptr){
+            libre[i] = 1;
+            return;
+        }
+    }
+    perror("Erreur leur du tempsreel_free!\n");
 }
